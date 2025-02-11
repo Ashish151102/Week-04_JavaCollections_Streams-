41361c1f54363b2ba59f7624f08e1642d9c3a7f6
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-11 22:08:49 +0530

    [Ashish] Add .program for Smart Warehouse Management System  using Generic Classes, Bounded Type Parameters, Wildcards.

---
+++ src/main/java/day01_javaGenerics/warehousemanagement/Storage.java
@@ -0,0 +1,23 @@
+package day01_javaGenerics.warehousemanagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item){
+        items.add(item);
+    }
+
+    public List<T> getItems(){
+        return  items;
+    }
+
+    public void displayList(List<? extends WarehouseItem> itemList){
+        for(WarehouseItem item : itemList){
+            System.out.println(item);
+        }
+    }
+}
+
