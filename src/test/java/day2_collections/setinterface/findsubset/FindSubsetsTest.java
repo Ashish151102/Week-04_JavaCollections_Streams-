9ad956dd84a177ffd709cf48d9a231729aa24a95
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-12 12:35:59 +0530

    [Ashish] Add .Implemented function to check if one set is a subset of another.

---
+++ src/test/java/day2_collections/setinterface/findsubset/FindSubsetsTest.java
@@ -0,0 +1,44 @@
+package day2_collections.setinterface.findsubset;
+
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindSubsetsTest {
+
+    @Test
+    void testAreSubsets_TrueCase() {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+        assertFalse(FindSubsets.areSubsets(set1, set2));
+    }
+
+    @Test
+    void testAreSubsets_FalseCase() {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(5, 6));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+        assertFalse(FindSubsets.areSubsets(set1, set2));
+    }
+
+    @Test
+    void testAreSubsets_EqualSets() {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3));
+        assertTrue(FindSubsets.areSubsets(set1, set2));
+    }
+
+    @Test
+    void testAreSubsets_EmptySubset() {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3));
+        assertFalse(FindSubsets.areSubsets(set1, set2));
+    }
+
+    @Test
+    void testAreSubsets_EmptySuperset() {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2));
+        Set<Integer> set2 = new HashSet<>();
+        assertFalse(FindSubsets.areSubsets(set1, set2));
+    }
+}
+
