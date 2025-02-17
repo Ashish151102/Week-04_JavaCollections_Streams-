24801effda9077f3e5d7cfe6e359fe902355b7fe
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-17 10:27:44 +0530

    [Ashish] Add. implemented a method isEven(int number) that returns true if a number is even Using @ParameterizedTest .

--- src/test/java/day5_junit/basicproblems/isEven/IsEvenTest.java
+++ src/test/java/day5_junit/basicproblems/isEven/IsEvenTest.java
@@ -2,25 +2,20 @@ package day5_junit.basicproblems.isEven;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class IsEvenTest {
 
-    @ParameterizedTest
-    @ValueSource(ints = {2, 4, 6, 8, 10})
-    void testIsEvenTrue(int number) {
-        assertTrue(IsEven.isEven(number), number + " should be even");
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {1, 3, 5, 7, 9})
-    void testIsEvenFalse(int number) {
-        assertFalse(IsEven.isEven(number), number + " should be odd");
+    @Test
+    void testEvenNumber() {
+        assertTrue(IsEven.isEven(4), "4 should be even");
+        assertTrue(IsEven.isEven(0), "0 should be even");
+        assertTrue(IsEven.isEven(-2), "-2 should be even");
     }
 
     @Test
-    void testIsEvenZero() {
-        assertTrue(IsEven.isEven(0), "0 should be even");
+    void testOddNumber() {
+        assertFalse(IsEven.isEven(3), "3 should be odd");
+        assertFalse(IsEven.isEven(-5), "-5 should be odd");
+        assertFalse(IsEven.isEven(7), "7 should be odd");
     }
 }
