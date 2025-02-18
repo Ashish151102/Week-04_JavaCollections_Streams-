d1ff0c4168d939188994862ebba8af3852f87ff8
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-18 12:51:55 +0530

    [Ashish] Add.  Add LegacyAPI class with deprecated oldFeature() method and newFeature() method.

---
+++ src/main/java/annotations/deprecated/Main.java
@@ -0,0 +1,8 @@
+package annotations.deprecated;
+public class Main {
+    public static void main(String[] args) {
+        LegacyAPI api = new LegacyAPI();
+        api.oldFeature();  // Will show a warning
+        api.newFeature();
+    }
+}
