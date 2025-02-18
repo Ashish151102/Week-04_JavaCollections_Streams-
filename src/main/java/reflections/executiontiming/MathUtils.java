2251de106d1dd3f100a983b92eb82dad53969a86
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-18 11:45:12 +0530

    [Ashish] Add. Implemented Reflection-based Method Execution Timing.

---
+++ src/main/java/reflections/executiontiming/MathUtils.java
@@ -0,0 +1,10 @@
+package reflections.executiontiming;
+public class MathUtils {
+    public void slowMethod() {
+        try { Thread.sleep(500); } catch (InterruptedException e) {}
+    }
+
+    public void fastMethod() {
+        try { Thread.sleep(100); } catch (InterruptedException e) {}
+    }
+}
---
+++ src/main/java/reflections/executiontiming/MethodTimer.java
@@ -0,0 +1,16 @@
+package reflections.executiontiming;
+import java.lang.reflect.Method;
+
+public class MethodTimer {
+    public static void main(String[] args) throws Exception {
+        MathUtils mathUtils = new MathUtils();
+        Method[] methods = MathUtils.class.getDeclaredMethods();
+
+        for (Method method : methods) {
+            long startTime = System.nanoTime();
+            method.invoke(mathUtils);
+            long endTime = System.nanoTime();
+            System.out.println(method.getName() + " executed in " + (endTime - startTime) / 1_000_000 + " ms");
+        }
+    }
+}
