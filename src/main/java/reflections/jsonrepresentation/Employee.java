88192de4e3409d5c87ecb135451b81b1f92ff9ed
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-18 11:59:55 +0530

    [Ashish] Add. Implemented JSON-like String Conversion Using Reflection.

---
+++ src/main/java/reflections/jsonrepresentation/Employee.java
@@ -0,0 +1,13 @@
+package reflections.jsonrepresentation;
+public class Employee {
+    private String name = "Alice";
+    private int salary = 50000;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
