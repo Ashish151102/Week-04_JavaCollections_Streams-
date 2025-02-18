c13452af4df28bbb09293326da018af09fbaa010
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-18 12:06:05 +0530

    [Ashish] Add. Implemented Custom Object Mapper Using Reflection.

---
+++ src/main/java/reflections/objectmapper/Person.java
@@ -0,0 +1,11 @@
+package reflections.objectmapper;
+
+class Person {
+    private String name;
+    private int age;
+
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
