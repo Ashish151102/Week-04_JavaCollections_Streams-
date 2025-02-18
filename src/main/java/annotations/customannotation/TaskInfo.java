2b4443343d806b47effdc704964bfef3eaecd6be
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-18 12:46:04 +0530

    [Ashish] Add.  Implemented a custom annotation @TaskInfo to mark tasks with priority and assigned person.

---
+++ src/main/java/annotations/customannotation/TaskInfo.java
@@ -0,0 +1,9 @@
+package annotations.customannotation;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface TaskInfo {
+    String assignedTo();
+    int priority();
+}
