6597a7f3da3d3e5314e86be3d7b7b5ad2f9ee8d0
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-18 13:40:20 +0530

    [Ashish] Add. 5️⃣ Implemented a Role-Based Access Control with @RoleAllowed , to restrict method access based on roles.

---
+++ src/main/java/annotations/roleallowed/UserService.java
@@ -0,0 +1,13 @@
+package annotations.roleallowed;
+public class UserService {
+
+    @RoleAllowed("ADMIN")
+    public void deleteUser() {
+        System.out.println("User deleted successfully.");
+    }
+
+    @RoleAllowed("USER")
+    public void viewProfile() {
+        System.out.println("User profile displayed.");
+    }
+}
